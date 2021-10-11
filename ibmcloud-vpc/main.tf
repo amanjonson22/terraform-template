dcfe90545d6fd03b808a6a13d0b92d882429bf22
Marcos Vieira <marcosbv@br.ibm.com>
2021-10-11 18:32:04 -0300

    Routing tables and address prefixes

--- ibmcloud-vpc/main.tf
+++ ibmcloud-vpc/main.tf
@@ -5,17 +5,27 @@
 locals {
     vpc_name = var.vpc_name
     resource_group_id = var.rg_id
-    vpc_subnet = var.vpc_subnet
     region = var.region
-    number_of_datacenters = var.number_of_datacenters
-    number_of_splits = var.number_of_splits
-    number_of_bits_ahead_subnet = local.number_of_splits + var.bits_ahead_subnet
+    zone1 = "${local.region}-1"
+    zone2 = "${local.region}-2"
+    zone3 = "${local.region}-3"
+
+    number_of_bits_ahead_subnet =  var.bits_ahead_subnet
 
     tags = var.tags
 
-    address_prefixes_dc1 = local.number_of_datacenters == 3 ? [cidrsubnet(local.vpc_subnet, local.number_of_splits, 0), cidrsubnet(cidrsubnet(local.vpc_subnet, local.number_of_splits, 3), 1, 0)] : [cidrsubnet(local.vpc_subnet, local.number_of_splits, 0)]
-    address_prefixes_dc2 = local.number_of_datacenters == 3 ? [cidrsubnet(local.vpc_subnet, local.number_of_splits, 1), cidrsubnet(cidrsubnet(local.vpc_subnet, local.number_of_splits, 3), 2, 2)] : [cidrsubnet(local.vpc_subnet, local.number_of_splits, 1)]
-    address_prefixes_dc3 = local.number_of_datacenters == 3 ? [cidrsubnet(local.vpc_subnet, local.number_of_splits, 2), cidrsubnet(cidrsubnet(local.vpc_subnet, local.number_of_splits, 3), 2, 3)] : []
+    address_prefixes_dc1 = var.address_prefixes[local.zone1]
+    address_prefixes_dc2 = var.address_prefixes[local.zone2]
+    address_prefixes_dc3 = var.address_prefixes[local.zone3]
+    private_subnets = var.private_subnets
+
+    subnets_dc1 = length(local.address_prefixes_dc1) > 0 ? local.private_subnets : []
+    subnets_dc2 = length(local.address_prefixes_dc2) > 0 ? local.private_subnets : []
+    subnets_dc3 = length(local.address_prefixes_dc3) > 0 ? local.private_subnets : []
+
+    routing_tables_dc1 = var.routing_tables[local.zone1]
+    routing_tables_dc2 = var.routing_tables[local.zone2]
+    routing_tables_dc3 = var.routing_tables[local.zone3]
 }
 
 resource ibm_is_vpc vpc {
@@ -29,7 +39,7 @@ resource ibm_is_vpc vpc {
 resource ibm_is_vpc_address_prefix address_prefix_dc1 {
     for_each = toset(local.address_prefixes_dc1)
     name = "vpc-address-prefix-${local.region}-dc1-${index(local.address_prefixes_dc1, each.key)}"
-    zone = "${local.region}-1"
+    zone = local.zone1
     vpc  = ibm_is_vpc.vpc.id
     cidr = each.key
 }
@@ -38,7 +48,7 @@ resource ibm_is_vpc_address_prefix address_prefix_dc1 {
 resource ibm_is_vpc_address_prefix address_prefix_dc2 {
     for_each = toset(local.address_prefixes_dc2)
     name = "vpc-address-prefix-${local.region}-dc2-${index(local.address_prefixes_dc2, each.key)}"
-    zone = "${local.region}-2"
+    zone = local.zone2
     vpc  = ibm_is_vpc.vpc.id
     cidr = each.key
 }
@@ -47,9 +57,69 @@ resource ibm_is_vpc_address_prefix address_prefix_dc2 {
 resource ibm_is_vpc_address_prefix address_prefix_dc3 {
     for_each = toset(local.address_prefixes_dc3)
     name = "vpc-address-prefix-${local.region}-dc3-${index(local.address_prefixes_dc3, each.key)}"
-    zone = "${local.region}-3"
+    zone = local.zone3
     vpc  = ibm_is_vpc.vpc.id
     cidr = each.key
+}
+
+/* Routing table (DC1) */
+resource "ibm_is_vpc_routing_table" "routing_table_dc1" {
+  vpc = ibm_is_vpc.vpc.id
+  name = "routing-table-${local.zone1}"
+  route_direct_link_ingress = false
+  route_transit_gateway_ingress = false
+  route_vpc_zone_ingress = false
+}
+
+resource "ibm_is_vpc_routing_table_route" "routes_dc1" {
+  for_each = toset(local.routing_tables_dc1)
+  vpc = ibm_is_vpc.vpc.id
+  routing_table = ibm_is_vpc_routing_table.routing_table_dc1.routing_table
+  zone = local.zone1
+  name = "custom-route-${index(local.routing_tables_dc1, each.key)}"
+  destination = split("-->", each.key)[0]
+  action = split("-->", each.key)[1] == "vpc" ? "delegate_vpc" : "deliver"
+  next_hop = split("-->", each.key)[1] == "vpc"? "0.0.0.0" : split("-->", each.key)[1] // Example value "10.0.0.4"
+}
+/* Routing table (DC2) */
+resource "ibm_is_vpc_routing_table" "routing_table_dc2" {
+  vpc = ibm_is_vpc.vpc.id
+  name = "routing-table-${local.zone2}"
+  route_direct_link_ingress = false
+  route_transit_gateway_ingress = false
+  route_vpc_zone_ingress = false
+}
+
+resource "ibm_is_vpc_routing_table_route" "routes_dc2" {
+  for_each = toset(local.routing_tables_dc2)
+  vpc = ibm_is_vpc.vpc.id
+  routing_table = ibm_is_vpc_routing_table.routing_table_dc2.routing_table
+  zone = local.zone2
+  name = "custom-route-${index(local.routing_tables_dc2, each.key)}"
+  destination = split("-->", each.key)[0]
+  action = split("-->", each.key)[1] == "vpc" ? "delegate_vpc" : "deliver"
+  next_hop = split("-->", each.key)[1] == "vpc"? "0.0.0.0" : split("-->", each.key)[1] // Example value "10.0.0.4"
+}
+
+/* Routing table (DC3) */
+
+resource "ibm_is_vpc_routing_table" "routing_table_dc3" {
+  vpc = ibm_is_vpc.vpc.id
+  name = "routing-table-${local.zone3}"
+  route_direct_link_ingress = false
+  route_transit_gateway_ingress = false
+  route_vpc_zone_ingress = false
+}
+
+resource "ibm_is_vpc_routing_table_route" "routes_dc3" {
+  for_each = toset(local.routing_tables_dc3)
+  vpc = ibm_is_vpc.vpc.id
+  routing_table = ibm_is_vpc_routing_table.routing_table_dc3.routing_table
+  zone = local.zone3
+  name = "custom-route-${index(local.routing_tables_dc3, each.key)}"
+  destination = split("-->", each.key)[0]
+  action = split("-->", each.key)[1] == "vpc" ? "delegate_vpc" : "deliver"
+  next_hop = split("-->", each.key)[1] == "vpc"? "0.0.0.0" : split("-->", each.key)[1] // Example value "10.0.0.4"
 }
 
 /*  Subnet (DC1)*/
@@ -57,11 +127,13 @@ resource ibm_is_subnet vpc_subnet_dc1 {
   depends_on = [
     ibm_is_vpc_address_prefix.address_prefix_dc1
   ]
-  for_each = toset(local.address_prefixes_dc1)
-  name            = "vpc-subnet-${local.region}-dc1-${index(local.address_prefixes_dc1, each.key)}"
+  for_each =      toset(local.subnets_dc1)
+  name            = "vpc-subnet-${local.region}-dc1-${each.key}"
+  //public_gateway  = each.key == "public" ? true : false 
   vpc             = ibm_is_vpc.vpc.id
-  zone = "${local.region}-1"
-  ipv4_cidr_block = cidrsubnet(each.key, local.number_of_bits_ahead_subnet, 0)
+  zone = local.zone1
+  ipv4_cidr_block = cidrsubnet(local.address_prefixes_dc1[0], local.number_of_bits_ahead_subnet, index(local.subnets_dc1, each.key))
+  routing_table = ibm_is_vpc_routing_table.routing_table_dc1.routing_table
 }
 
 /*  Subnet (DC2)*/
@@ -69,11 +141,13 @@ resource ibm_is_subnet vpc_subnet_dc2 {
   depends_on = [
     ibm_is_vpc_address_prefix.address_prefix_dc2
   ]
-  for_each = toset(local.address_prefixes_dc2)
-  name            = "vpc-subnet-${local.region}-dc2-${index(local.address_prefixes_dc2, each.key)}"
+  for_each =      toset(local.subnets_dc2)
+  name            = "vpc-subnet-${local.region}-dc2-${each.key}"
+ // public_gateway  = each.key == "public" ? true : false 
   vpc             = ibm_is_vpc.vpc.id
-  zone = "${local.region}-2"
-  ipv4_cidr_block = cidrsubnet(each.key, local.number_of_bits_ahead_subnet, 0)
+  zone = local.zone2
+  ipv4_cidr_block = cidrsubnet(local.address_prefixes_dc2[0], local.number_of_bits_ahead_subnet, index(local.subnets_dc2, each.key))
+  routing_table = ibm_is_vpc_routing_table.routing_table_dc2.routing_table
 }
 
 /*  Subnet (DC3)*/
@@ -81,9 +155,10 @@ resource ibm_is_subnet vpc_subnet_dc3 {
   depends_on = [
     ibm_is_vpc_address_prefix.address_prefix_dc3
   ]
-  for_each = toset(local.address_prefixes_dc3)
-  name            = "vpc-subnet-${local.region}-dc3-${index(local.address_prefixes_dc3, each.key)}"
+  for_each =      toset(local.subnets_dc3)
+  name            = "vpc-subnet-${local.region}-dc3-${each.key}"
+//public_gateway  = each.key == "public" ? true : false 
   vpc             = ibm_is_vpc.vpc.id
-  zone = "${local.region}-3"
-  ipv4_cidr_block = cidrsubnet(each.key, local.number_of_bits_ahead_subnet, 0)
+  zone = local.zone3
+  ipv4_cidr_block = cidrsubnet(local.address_prefixes_dc3[0], local.number_of_bits_ahead_subnet, index(local.subnets_dc3, each.key))
 }
--- ibmcloud-vpc/variables.tf
+++ ibmcloud-vpc/variables.tf
@@ -17,18 +17,6 @@ variable region {
     default = "br-sao"
 }
 
-variable number_of_datacenters {
-    description = "Number of availability zones to provision the VPC"
-    type = number
-    validation {
-        condition = var.number_of_datacenters <= 3
-        error_message = "The maximum of availability zones is 3."
-    }
-}
-
-variable number_of_splits {
-    description = "Number of bits ahead CIDR to break down the address prefixes"
-}
 variable bits_ahead_subnet {
     type = number
     description = "Number of bits for a subnet related to address prefix (i.e: if address_prefix is a /24 and this variable is 2, the subnet will be created as /26)"
@@ -37,5 +25,26 @@ variable bits_ahead_subnet {
 variable tags {
     type = list(string)
     default = []
+    description = "Tags to be used in the VPC objects"
 }
 
+variable private_subnets {
+    type = list(string)
+    default = []
+    description = "Name of private subnets to create. They will be allocated in the first address prefix."
+}
+
+variable address_prefixes {
+    type = map(list(string))
+    description = "Set of address prefixes to set to the datacenter regions."
+}
+
+variable routing_tables {
+    type = map(list(string))
+    default = {
+       "br-sao-1" =[],
+       "br-sao-2" = [],
+       "br-sao-3" = []
+    }
+    description = "List of routing tables per datacenter. Each entry will be a map like this example: {cidr = '192.168.0.1/24' route_to = '192.168.0.254 or vpc to delegate to vpc'}"
+}
