79d1f3058eeb99c857948d2e57dab38f9de9df7c
Marcos Vieira <marcosbv@br.ibm.com>
2021-11-11 10:09:04 -0300

    Initial commit of multizone Checkpoint firewall

---
+++ ibmcloud-multizone-checkpoint-firewall/main.tf
@@ -0,0 +1,102 @@
+/* Local Variables */
+locals {
+    resource_group = var.resource_group
+    vpc_subnet_ids = var.vpc_subnet_ids
+    vpc_zones = var.vpc_zones
+
+    vpc_name = var.vpc_name
+
+    vpc_first_zone = length(local.vpc_zones) > 0 ? local.vpc_zones[0] : ""
+    region = local.vpc_first_zone == "" ? "" : "${split("-",local.vpc_first_zone)[0]}-${split("-",local.vpc_first_zone)[1]}"
+
+    firewall_ssh_key = var.firewall_ssh_key
+    firewall_security_groups = var.firewall_security_groups
+
+    health_check_fw_port = 8117
+    firewall_version = "R81"
+    
+    firewall_profile = var.firewall_profile
+    firewall_prefix_name = var.firewall_prefix_name
+    load_balancer_prefix_name = var.load_balancer_prefix_name
+
+    load_balancer_public_ip = var.load_balancer_public_ip
+
+    load_balancer_type = local.load_balancer_public_ip == true ? "public" : "private"
+    load_balancer_route_mode = local.load_balancer_public_ip == true ? false : true
+
+    tags = var.tags
+}
+
+data ibm_resource_group resource_group {
+    name = local.resource_group
+}
+
+/* Firewall Checkpoint Module (for_each DC) */
+module gateways {
+    for_each = toset(local.vpc_zones)
+    source = "../../checkpoint-iaas-cluster-ibm-vpc"
+    VPC_Region = local.region
+    VPC_Name = local.vpc_name
+    Resource_Group = local.resource_group
+    External_Subnet_ID = local.vpc_subnet_ids[index(local.vpc_zones, each.key)]
+    CP_Version = local.firewall_version
+    SSH_Key = local.firewall_ssh_key
+    VNF_Security_Group = local.firewall_security_groups[index(local.vpc_zones, each.key)]
+    VNF_Profile = local.firewall_profile
+    VNF_CP-GW_Instance1 = "${local.firewall_prefix_name}-${each.key}-primary"
+    VNF_CP-GW_Instance2 = "${local.firewall_prefix_name}-${each.key}-secondary"
+    tags = local.tags
+}
+
+/* Network Load Balancer (for_each DC) */
+resource ibm_is_lb load_balancer {
+    for_each = toset(local.vpc_zones)
+    name = "${local.load_balancer_prefix_name}-${each.key}"
+    subnets = [local.vpc_subnet_ids[index(local.vpc_zones, each.key)]]
+    // future attribute according to PR https://github.com/IBM-Cloud/terraform-provider-ibm/pull/3208
+    route_mode = local.load_balancer_route_mode
+    profile = "network-fixed"
+    type = local.load_balancer_type
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags = local.tags
+}
+
+/* Network Load Balancer Pool and Pool Members (for_each DC) */
+resource ibm_is_lb_pool load_balancer_pool {
+    for_each = toset(local.vpc_zones)
+    name = "${local.load_balancer_prefix_name}-${each.key}-pool"
+    lb = ibm_is_lb.load_balancer[each.key].id
+    algorithm = "round_robin"
+    protocol = "tcp"
+    health_type = "tcp"
+    health_monitor_port = local.health_check_fw_port
+    health_retries = 3
+    health_delay = 5
+    health_timeout = 2  
+}
+
+resource ibm_is_lb_pool_member load_balancer_primary_members {
+   for_each = toset(local.vpc_zones)
+   lb = ibm_is_lb.load_balancer[each.key].id
+   pool = ibm_is_lb_pool.load_balancer_pool[each.key].id
+   port = 90
+   target_id = module.gateways[each.key].firewall_instance_ids[0]
+}
+
+resource ibm_is_lb_pool_member load_balancer_secondary_members {
+   for_each = toset(local.vpc_zones)
+   lb = ibm_is_lb.load_balancer[each.key].id
+   pool = ibm_is_lb_pool.load_balancer_pool[each.key].id
+   port = 90
+   target_id = module.gateways[each.key].firewall_instance_ids[1]
+}
+
+/* Network Load Balancer Listener (for_each DC) */ 
+resource ibm_is_lb_listener load_balancer_listener {
+  for_each = toset(local.vpc_zones)
+  lb       = ibm_is_lb.load_balancer[each.key].id
+  //port     = 1
+  // future parameters for PR https://github.com/IBM-Cloud/terraform-provider-ibm/pull/3207 (port_min and port_max will be calculated for routing mode)
+  protocol = "tcp"
+  default_pool = ibm_is_lb_pool.load_balancer_pool[each.key].pool_id
+}
---
+++ ibmcloud-multizone-checkpoint-firewall/variables.tf
@@ -0,0 +1,55 @@
+variable ibmcloud_api_key{}
+
+variable vpc_zones {
+    type = list(string)
+    description = "List of zones to apply this Checkpoint Active/Active firewall."
+}
+
+variable vpc_name {
+    type = string
+    description = "VPC Name to deploy the firewalls into."
+}
+
+variable vpc_subnet_ids {
+    type = list(string)
+    description = "List of subnet IDs to apply this Checkpoint Active/Active firewall."
+}
+
+variable resource_group {
+    description = "Name of resource group to insert the firewall resources into."
+}
+
+variable firewall_ssh_key {
+    description = "SSH Key Name created in VPC to insert into the firewalls."
+}
+
+variable firewall_security_groups {
+    type = list(string)
+    description = "List of security groups for the created firewalls."
+}
+
+variable firewall_profile {
+    default = "cx2-8x16"
+    description = "Machine profile for the firewalls." 
+}
+
+variable firewall_prefix_name {
+    description = "Prefix name to be used for the firewalls to be deployed."
+     default = "fw-checkpoint"
+}
+variable load_balancer_public_ip {
+    type = bool
+    default = false
+    description = "True if load balancers should be created by using public IPs."
+}
+
+variable load_balancer_prefix_name {
+    description = "Prefix name for the load balancer."
+    default = "lb-diti"
+}
+
+variable tags {
+    type = list(string)
+    default = []
+    description = "Tags to insert on provisioned resources"
+}
